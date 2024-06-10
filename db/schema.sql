94fa3b80d6dd276a2647a629cfccb8278dfd27f1
Shaniqua Coston <shaniquacoston@pursuit.org>
2024-06-10 19:55:53 -0400

    changed price to an INT

--- db/schema.sql
+++ db/schema.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS menu_items;
 CREATE TABLE menu_items (
     id SERIAL PRIMARY KEY,
     item_name VARCHAR(255) NOT NULL,
-    price VARCHAR(255) NOT NULL,
+    price INT NOT NULL,
     description TEXT,
     image_url VARCHAR(255),
     inventory_count INT,
